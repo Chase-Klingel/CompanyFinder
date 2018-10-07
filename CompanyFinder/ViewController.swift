295abb555b91cbabeabf53d828af38f7a9d9b919
Chase-Klingel <klingelchase@gmail.com>
2018-10-07 09:58:14 -0700

    Add initial UI style to "companies" view

--- CompanyFinder/ViewController.swift
+++ CompanyFinder/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        navigationItem.title = "Companies"
+        
+        setupNavigationStyle()
     }
+    
+    private func setupNavigationStyle() {
+        let lightRed = UIColor(red: 247/255, green: 66/255,
+                               blue: 82/255, alpha: 1)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes =
+            [NSAttributedStringKey.foregroundColor: UIColor.white]
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal),
+                            style: .plain, target: self, action: #selector(handleAddCompany))
+        
     }
-
-
+    
+    @objc private func handleAddCompany() {
+        print("adding company")
+    }
 }
 
