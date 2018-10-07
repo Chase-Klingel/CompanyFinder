89a1b73bfccaa2b2d3992fd77dc162832533b05f
Chase-Klingel <klingelchase@gmail.com>
2018-10-07 10:29:31 -0700

    Add UIColor extension for "theme colors" and table view outline

---
+++ CompanyFinder/UIColor+theme.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+theme.swift
+//  CompanyFinder
+//
+//  Created by Chase Klingel on 10/7/18.
+//  Copyright © 2018 Chase Klingel. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let tealColor = UIColor(red: 48/255, green: 164/255,
+                                   blue: 182/255, alpha: 1)
+    
+    static let lightRed = UIColor(red: 247/255, green: 66/255,
+                           blue: 82/255, alpha: 1)
+    
+    static let darkBlue = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+    
+    // UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+}
--- CompanyFinder/ViewController.swift
+++ CompanyFinder/ViewController.swift
@@ -8,26 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationItem.title = "Companies"
+        tableView.backgroundColor = .darkBlue
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
         
         setupNavigationStyle()
     }
     
     private func setupNavigationStyle() {
-        let lightRed = UIColor(red: 247/255, green: 66/255,
-                               blue: 82/255, alpha: 1)
+        
 
         navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.barTintColor = lightRed
+        
+        navigationController?.navigationBar.barTintColor = .lightRed
+        
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // handles small title text
+        navigationController?.navigationBar.titleTextAttributes =
+            [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
         navigationController?.navigationBar.largeTitleTextAttributes =
             [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
         navigationItem.rightBarButtonItem =
             UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal),
                             style: .plain, target: self, action: #selector(handleAddCompany))
@@ -36,6 +49,20 @@ class ViewController: UIViewController {
     
     @objc private func handleAddCompany() {
         print("adding company")
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .tealColor
+        
+        return cell
     }
 }
 
